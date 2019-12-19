d82010ed1763f77d4c96387f8cefd042fa4f80d7
wing <wing@mailtime.com>
2019-12-19 15:05:40 +0800

    revert demo code and remove not required file

--- src/pages/press.js
+++ src/pages/press.js
@@ -2,42 +2,12 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { Link } from "gatsby"
 
-const PressPage = ({ data }) => {
-  return (
-    <Layout>
-      <SEO title="Press" />
-      <h1>Press page</h1>
-      <div>
-        <h4>{data.allWordpressPost.edges.length} Posts</h4>
-        {data.allWordpressPost.edges.map(({ node }) => (
-          <div key={node.id}>
-            <Link to={node.slug}>
-              <p>{node.title}</p>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </Layout>
-  )
-}
-
-export const query = graphql`
-  query {
-    allWordpressPost {
-      edges {
-        node {
-          id
-          content
-          path
-          slug
-          title
-          wordpress_id
-        }
-      }
-    }
-  }
-`
+const PressPage = () => (
+  <Layout>
+    <SEO title="Press" />
+    <h1>Press page</h1>
+  </Layout>
+)
 
 export default PressPage
