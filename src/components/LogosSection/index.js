a71d1e609b856c4230a2795243675a40505c554a
wing <wing@mailtime.com>
2020-01-03 14:17:54 +0800

    1. using useStaticQuery to query image 2. all image in logos section change to .png

--- src/components/LogosSection/index.js
+++ src/components/LogosSection/index.js
@@ -1,112 +1,40 @@
 import React from "react"
 import { FormattedMessage } from "gatsby-plugin-intl"
 
+import Achievements from "./Achievements"
+import BackedBy from "./BackedBy"
+import TrustedBy from "./TrustedBy"
+
 import {
   container,
   inlineContainer,
   achievementsContainer,
   backedByContainer,
   trustedByContainer,
-  achievementsImage,
-  trustedByImage,
-  backedByImage,
-  imageContainer,
-  imageContainer1,
-  imageContainer2,
-  imageContainer3,
   title,
-  andMoreContainer1,
-  andMoreContainer2,
-  andMoreContainer3,
-  andMore,
 } from "./style"
-import achievements01 from "@/images/achievements01.svg"
-import achievements02 from "@/images/achievements02.svg"
-import achievements03 from "@/images/achievements03.svg"
-import achievements04 from "@/images/achievements04.svg"
-import achievements05 from "@/images/achievements05.svg"
-import achievements06 from "@/images/achievements06.svg"
-import achievements07 from "@/images/achievements07.svg"
-import GatsbyImage from "@/components/GatsbyImage"
 
-const AchievementsImagesArr = [
-  achievements01,
-  achievements02,
-  achievements03,
-  achievements04,
-  achievements05,
-  achievements06,
-  achievements07,
-]
-
-const BackedByImagesArr = [
-  "backedby01",
-  "backedby02",
-  "backedby03",
-  "backedby04",
-  "backedby05",
-  "backedby06",
-  "backedby07",
-]
-
-const TrustedByImagesArr = ["trustedby01", "trustedby02"]
-
-const AndMore = () => (
-  <p css={andMore}>
-    <FormattedMessage id="andMore" defaultMessage="and more..." />
-  </p>
-)
+const Title = ({ children }) => <p css={title}>{children}</p>
 
 const LogosSection = props => (
   <div css={container} {...props}>
     <div css={[inlineContainer, achievementsContainer]}>
-      <p css={title}>
+      <Title>
         <FormattedMessage id="achievements" defaultMessage="ACHIEVEMENTS" />
-      </p>
-      <div css={[imageContainer, imageContainer1]}>
-        {AchievementsImagesArr.map(image => (
-          <img css={achievementsImage} src={image} alt={image} {...props} />
-        ))}
-        <div css={andMoreContainer1}>
-          <AndMore />
-        </div>
-      </div>
+      </Title>
+      <Achievements />
     </div>
     <div css={[inlineContainer, backedByContainer]}>
-      <p css={title}>
+      <Title>
         <FormattedMessage id="backedBy" defaultMessage="BACKED BY" />
-      </p>
-      <div css={[imageContainer, imageContainer2]}>
-        {BackedByImagesArr.map(image => (
-          <GatsbyImage
-            objectFit="contain"
-            css={backedByImage}
-            filename={image + ".png"}
-            alt={image}
-          />
-        ))}
-        <div css={andMoreContainer2}>
-          <AndMore />
-        </div>
-      </div>
+      </Title>
+      <BackedBy />
     </div>
     <div css={[inlineContainer, trustedByContainer]}>
-      <p css={title}>
+      <Title>
         <FormattedMessage id="trustedBy" defaultMessage="TRUSTED BY" />
-      </p>
-      <div css={[imageContainer, imageContainer3]}>
-        {TrustedByImagesArr.map(image => (
-          <GatsbyImage
-            objectFit="contain"
-            css={trustedByImage}
-            filename={image + ".png"}
-            alt={image}
-          />
-        ))}
-        <div css={andMoreContainer3}>
-          <AndMore />
-        </div>
-      </div>
+      </Title>
+      <TrustedBy />
     </div>
   </div>
 )
--- src/components/LogosSection/style.js
+++ src/components/LogosSection/style.js
@@ -1,6 +1,7 @@
 import { css } from "@emotion/core"
 
 export const container = css`
+  display: flex;
   position: relative;
   z-index: 1;
   padding: 4rem 1.2rem;
@@ -11,71 +12,20 @@ export const inlineContainer = css`
 `
 
 export const achievementsContainer = css`
-  height: 46.3rem;
-  width: 28.5rem;
   margin-right: 9.9rem;
 `
 export const backedByContainer = css`
   top: 6.6rem;
-  height: 39.7rem;
-  width: 30.5rem;
   margin-right: 9.9rem;
 `
 export const trustedByContainer = css`
   top: 13.1rem;
-  height: 33.2rem;
-  width: 17.3rem;
-`
-
-export const imageContainer = css`
-  display: flex;
-  flex-wrap: wrap;
-  align-content: space-between;
-  justify-content: space-between;
-  height: 100%;
-`
-
-export const achievementsImage = css`
-  width: fit-content;
-  height: fit-content;
-  min-width: 11.5rem;
-  max-height: 6.8rem;
-`
-export const backedByImage = css`
-  width: 100%;
-  max-width: 13rem;
-`
-export const trustedByImage = css`
-  width: 100%;
-  max-width: 13rem;
-  max-height: 3.6rem;
-`
-
-export const imageContainer1 = css``
-export const imageContainer2 = css``
-export const imageContainer3 = css`
-  justify-content: center;
 `
 
 export const title = theme => css`
-  margin: 0 0 3.3rem 0;
+  margin: 0;
+  margin-bottom: 3.3rem;
   font-size: 2.8rem;
   letter-spacing: 0.6px;
   color: ${theme.colors.purples.light};
-`
-export const andMoreContainer1 = css`
-  width: 12.2rem;
-`
-export const andMoreContainer2 = css`
-  width: 13.4rem;
-`
-export const andMoreContainer3 = css`
-  width: 17.3rem;
-`
-export const andMore = theme => css`
-  margin: 0 auto;
-  font-size: 1.2rem;
-  letter-spacing: 0.35px;
-  color: ${theme.colors.purples.light};
-  text-align: center;
 `
