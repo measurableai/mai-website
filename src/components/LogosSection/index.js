8685867d2ef5a5615db91c9b174bf494f47d0276
wing <wing@mailtime.com>
2020-01-14 10:59:17 +0800

    real camel case

--- src/components/LogosSection/index.js
+++ src/components/LogosSection/index.js
@@ -161,7 +161,7 @@ const LogosSection = props => {
 
   const trustedByColumnCount = useMedia([2], 1)
   const intl = useIntl()
-  const ENGLISH_VERSION = intl.locale === "en-US"
+  const englishVersion = intl.locale === "en-US"
   return (
     <div css={container} {...props}>
       <Category
@@ -188,14 +188,14 @@ const LogosSection = props => {
           images.backedby01.childImageSharp.fluid,
           images.backedby02.childImageSharp.fluid,
           images.backedby03.childImageSharp.fluid,
-          ENGLISH_VERSION
+          englishVersion
             ? images.backedby04.childImageSharp.fluid
             : images.backedby04_cn.childImageSharp.fluid,
           images.backedby05.childImageSharp.fluid,
-          ENGLISH_VERSION
+          englishVersion
             ? images.backedby06.childImageSharp.fluid
             : images.backedby06_cn.childImageSharp.fluid,
-          ENGLISH_VERSION
+          englishVersion
             ? images.backedby07.childImageSharp.fluid
             : images.backedby07_cn.childImageSharp.fluid,
         ]}
@@ -206,7 +206,7 @@ const LogosSection = props => {
         columnCount={trustedByColumnCount}
         images={[
           images.trustedby01.childImageSharp.fluid,
-          ENGLISH_VERSION
+          englishVersion
             ? images.trustedby02.childImageSharp.fluid
             : images.trustedby02_cn.childImageSharp.fluid,
         ]}
