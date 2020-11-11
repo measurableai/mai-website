029d1b9b61000e09ca1ecc9e16050a0a602e6602
wing <wing@mailtime.com>
2020-11-11 18:20:00 +0800

    remove unused code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -26,10 +26,7 @@ import mailogo from "@/images/mai-logo-2.svg"
 import mdtLogo from "@/images/mdt-logo.svg"
 import facebookIcon from "@/images/btn-facebook.svg"
 import linkedinIcon from "@/images/btn-linkedin.svg"
-// import mediumIcon from "@/images/btn-medium.svg"
 import twitterIcon from "@/images/btn-twitter.svg"
-// import wechatIcon from "@/images/btn-wechat.svg"
-// import weiboIcon from "@/images/btn-weibo.svg"
 
 import useMedia from "@/hooks/useMedia"
 
@@ -81,11 +78,6 @@ const Footer = () => {
                 alt="linkedin"
                 href="https://linkedin.com/company/measurable-ai"
               />
-              {/* <SocialIcon
-                src={mediumIcon}
-                alt="medium"
-                href="https://medium.com/measurable-ai"
-              /> */}
               <SocialIcon
                 src={twitterIcon}
                 alt="twitter"
@@ -95,14 +87,8 @@ const Footer = () => {
                 src={facebookIcon}
                 alt="facebook"
                 href="https://fb.me/measurableai"
-              />
-              {/* <SocialIcon src={wechatIcon} alt="wechat" /> */}
-              {/* <SocialIcon
-                src={weiboIcon}
-                alt="weibo"
-                href="https://weibo.com/measurableai"
                 last={true}
-              /> */}
+              />
             </div>
           </div>
           <div css={contentRightContainer}>
