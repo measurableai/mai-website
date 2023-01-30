581d1302e482f822eb855803c14390426b36287b
Maxi Cho <chochihim@gmail.com>
2023-01-30 16:04:26 +0800

    un-hide `<AnnualReportForm />`

--- src/pages/annual-report.js
+++ src/pages/annual-report.js
@@ -43,7 +43,7 @@ const AnnualReportRequestPage = () => {
         <AnnualReportHero css={shoutOutPadding} />
       </SlopedSection>
       <AnnualReportDetailSection />
-      {/* <AnnualReportForm /> */}
+      <AnnualReportForm />
     </Layout>
   )
 }
