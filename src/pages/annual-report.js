fb9a38cc22cd1282c0bedcc0e8dd2ad37258434e
fontofonto <donlam1994@gmail.com>
2023-01-31 01:46:43 -0800

    add open graph image plugin and the related image

--- src/pages/annual-report.js
+++ src/pages/annual-report.js
@@ -9,6 +9,8 @@ import AnnualReportDetailSection from "@/components/AnnualReport/DetailSection/i
 import AnnualReportForm from "@/components/AnnualReport/Form"
 import AnnualReportHero from "@/components/AnnualReport/Hero"
 import "@/fonts/barlow.css"
+import Helmet from "react-helmet"
+import ogImageJpg from "@/images/annual-report/og-image.jpg"
 
 const background = theme => css`
   padding: 0;
@@ -35,7 +37,13 @@ const AnnualReportRequestPage = () => {
 
   return (
     <Layout headerMode="light">
-      <SEO title="Asia Online Delivery Annual Report 2019-2022" />
+      <SEO
+        title="Asia Online Delivery Annual Report 2019-2022"
+        description="This Report provides an overview of the online delivery (food + grocery) market across 9 regions: Singapore, Malaysia, Thailand, Indonesia, Hong Kong, Taiwan, Philippines, India and Japan. See which players and countries have been performing relatively well throughout the years 2019 to 2022."
+      />
+      <Helmet>
+        <meta property="og:image" content={ogImageJpg} />
+      </Helmet>
       <SlopedSection
         css={background}
         slopedBackgroundImage={theme.linearGradients.greenDarkToLight}
