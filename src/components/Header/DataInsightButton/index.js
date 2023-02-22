4c2b7bf360966cda594ec1a045fffbca45e30ab8
fontofonto <donlam1994@gmail.com>
2023-02-21 21:09:27 -0800

    Rewrite the sending gtag event function in Data Insight Button

--- src/components/Header/DataInsightButton/index.js
+++ src/components/Header/DataInsightButton/index.js
@@ -1,37 +1,29 @@
-import React from "react"
+import React, { useCallback } from "react"
 import { FormattedMessage } from "gatsby-plugin-intl"
 import { OutboundLink } from "gatsby-plugin-google-gtag"
 
 import TextButton from "@/components/TextButton"
 
-const gtag_report_conversion = url => {
-  // window.gtag is undefined in development & do not track is true
-  if (typeof window.gtag !== "undefined") {
-    console.log("Send Gtag Event.")
-    window.gtag("event", "conversion", {
-      send_to: "AW-11082494271/DdFtCIv3z4wYEL_ixaQp",
-    })
-  }
-
-  return false
-}
-
 const LoginButton = ({ location, ...props }) => {
-  let isReportPage = false
-  const pathname = typeof window !== "undefined" ? window.location.pathname : ""
+  const gtag_report_conversion = useCallback(() => {
+    const pathname =
+      typeof window !== "undefined" ? window.location.pathname : ""
 
-  if (pathname.includes("asia-food-delivery-report")) {
-    isReportPage = true
-  } else {
-    isReportPage = false
-  }
+    if (pathname.includes("asia-food-delivery-report")) {
+      if (typeof window.gtag !== "undefined") {
+        window.gtag("event", "conversion", {
+          send_to: "AW-11082494271/DdFtCIv3z4wYEL_ixaQp",
+        })
+      }
+    }
+  }, [])
 
   return (
     <OutboundLink
       href="https://blog.measurable.ai/"
       target="_blank"
       rel="noopener noreferrer"
-      onClick={isReportPage ? () => gtag_report_conversion() : null}
+      onClick={() => gtag_report_conversion()}
     >
       <TextButton {...props}>
         <FormattedMessage id="dataInsights" defaultMessage="Data Insights" />
