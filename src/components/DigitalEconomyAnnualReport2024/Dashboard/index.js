179928722ea6d21e42362aeae18fa00f4fd6d8da
fontofonto <donlam1994@gmail.com>
2024-04-09 02:14:20 -0700

    customized height of iframe depend on the screen size

--- src/components/DigitalEconomyAnnualReport2024/Dashboard/index.js
+++ src/components/DigitalEconomyAnnualReport2024/Dashboard/index.js
@@ -10,7 +10,6 @@ const AnnualReportDashboardIframe = () => {
         id="maiAnnualReportDashboard"
         title="MAI Annual Report"
         width={"100%"}
-        height={"768"}
         src="https://hub.upsolve.ai/share/dashboard/4c67e41b-1ee4-413a-a8ec-776dc4917bb7?theme=light"
         frameBorder={0}
       ></iframe>
--- src/components/DigitalEconomyAnnualReport2024/Dashboard/style.js
+++ src/components/DigitalEconomyAnnualReport2024/Dashboard/style.js
@@ -1,19 +1,34 @@
 import { css } from "@emotion/core"
+import { mq } from "@/theme"
 
 export const DashboardWrapper = theme => css`
-  width: 80%;
-  margin: 80px auto;
+  width: 100%;
+  margin: 120px auto;
   position: relative;
   z-index: 2;
+
+  ${mq.tablet} {
+    width: 90%;
+  }
 `
 
 export const Dashboard = theme => css`
   border: 2px solid #0a0a80;
   border-radius: 16px;
   transition: all 200ms ease;
+  height: 40vw;
+  transform: scale(0.95);
 
   :hover {
     border-radius: 8px;
+    transform: scale(1);
+  }
+
+  ${mq.tablet} {
+    transform: scale(1);
+    border-radius: 8px;
+    transition: none;
+    height: 80vh;
   }
 `
 
@@ -24,6 +39,11 @@ export const UnlockBtn = theme => css`
   justify-content: center;
   padding: 1.6rem;
   text-decoration: none;
+
+  img {
+    width: 4.8rem;
+    height: 4.8rem;
+  }
 
   span {
     font-family: "Barlow";
@@ -39,5 +59,15 @@ export const UnlockBtn = theme => css`
 
   :hover span {
     color: #00d2c8;
+  }
+
+  ${mq.tablet} {
+    img {
+      width: 3.2rem;
+      height: 3.2rem;
+    }
+    span {
+      font-size: 1.8rem;
+    }
   }
 `
