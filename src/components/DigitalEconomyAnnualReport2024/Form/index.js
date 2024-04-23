aa0f1b5ecc34f20c7279dd94245d5755cec87921
fontofonto <donlam1994@gmail.com>
2024-04-22 23:46:30 -0700

    Update the sign up text for launching

--- src/components/DigitalEconomyAnnualReport2024/Form/index.js
+++ src/components/DigitalEconomyAnnualReport2024/Form/index.js
@@ -74,8 +74,8 @@ const AnnualReportForm = () => {
     <section css={formSection}>
       <div css={signUpIntroContent}>
         <h2>
-          <span className="cyan-text uppercase">Sign Up</span> for Early Access{" "}
-          <span className="uppercase">NOW</span> -
+          <span className="cyan-text uppercase">Sign Up</span> to{" "}
+          <span className="uppercase">RECEIVE REPORT NOW</span>
         </h2>
         <p>
           Sign Up for the 2024 Digital Economy Annual Report: Emerging Markets
