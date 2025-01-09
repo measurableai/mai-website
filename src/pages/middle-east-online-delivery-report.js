e99b2617932918e66eb8afff646b56361e8fc186
fontofonto <donlam1994@gmail.com>
2025-01-09 01:52:31 -0800

    Move the SEO component out from Layout component

--- src/pages/middle-east-online-delivery-report.js
+++ src/pages/middle-east-online-delivery-report.js
@@ -59,7 +59,7 @@ const background = css`
 
 const MiddleEastOnlineDeliveryReportPage = () => {
   return (
-    <Layout headerMode="light">
+    <>
       <SEO
         title="Middle East Online Delivery Report 2020 - 2024: Food + Grocery"
         description="Measurable AI’s latest report on middle east online delivery industry 2020-2024 across Saudi Arabia, United Arab Emirates, Qatar, Turkey, Egypt, Kuwait."
@@ -83,43 +83,45 @@ const MiddleEastOnlineDeliveryReportPage = () => {
           },
         ]}
       />
-      <div css={background}>
-        <svg
-          viewBox="0 0 1920 1121"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
-            d="M0 0H1920V364L0 1121V0Z"
-            fill="url(#paint0_linear_1894_4622)"
-          />
-          <defs>
-            <linearGradient
-              id="paint0_linear_1894_4622"
-              x1="926.5"
-              y1="254.345"
-              x2="-142.831"
-              y2="1529.74"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop offset="0.230902" stop-color="#00F9D8" stop-opacity="0" />
-              <stop offset="1" stop-color="#00F9D8" stop-opacity="0.24" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
-
-      <div css={layout}>
-        <div>
-          <ReportBody />
+      <Layout headerMode="light">
+        <div css={background}>
+          <svg
+            viewBox="0 0 1920 1121"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M0 0H1920V364L0 1121V0Z"
+              fill="url(#paint0_linear_1894_4622)"
+            />
+            <defs>
+              <linearGradient
+                id="paint0_linear_1894_4622"
+                x1="926.5"
+                y1="254.345"
+                x2="-142.831"
+                y2="1529.74"
+                gradientUnits="userSpaceOnUse"
+              >
+                <stop offset="0.230902" stop-color="#00F9D8" stop-opacity="0" />
+                <stop offset="1" stop-color="#00F9D8" stop-opacity="0.24" />
+              </linearGradient>
+            </defs>
+          </svg>
         </div>
-        <aside>
-          <AnnualReportForm />
-        </aside>
-      </div>
-    </Layout>
+
+        <div css={layout}>
+          <div>
+            <ReportBody />
+          </div>
+          <aside>
+            <AnnualReportForm />
+          </aside>
+        </div>
+      </Layout>
+    </>
   )
 }
 
